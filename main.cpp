741f10810cacb01be9fee95820c5e74589163f07
jonathanaugusto.rjbr@gmail.com <jonathanaugusto.rjbr@gmail.com@b952d87f-6ffc-8d0e-2398-ceac7c6cec8f>
2011-09-09 03:19:44 +0000

    Main function

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "Includes.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	Node node1 (1);
+	Node node2 (2);
+	Node node3 (3);
+	Node node4 (4);
+
+	Edge edge1 (&node1, &node2, 1, false);
+	Edge edge2 (&node1, &node4, 1, false);
+	Edge edge3 (&node2, &node3, 1, false);
+
+	system ("PAUSE");
+    return 0;
+}
+
