68d2f06fe39b2c6994bfb9247e8a2b00a5d37fe5
jonathanaugusto.rjbr@gmail.com <jonathanaugusto.rjbr@gmail.com@b952d87f-6ffc-8d0e-2398-ceac7c6cec8f>
2011-09-09 03:17:50 +0000

    Includes header

---
+++ Includes.h
@@ -0,0 +1,27 @@
+/*
+ * includes.h
+ *
+ *  Created on: 08/09/2011
+ *      Author: Jonny
+ */
+
+#ifndef INCLUDES_H_
+#define INCLUDES_H_
+
+#pragma once  // Para resolver a referencia cruzada (Node inclui Edge, que inclui Node...)
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <cstdlib>
+#include <initializer_list>
+#include <list>
+#include <algorithm>
+#include "Edge.h"
+#include "Node.h"
+#include "Graph.h"
+
+#endif /* INCLUDES_H_ */
